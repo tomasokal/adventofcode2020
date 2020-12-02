7fc6a51860c308eb7a1a1d3964881fdb78648ee9
Tomas Okal <tomas.okal@gmail.com>
2020-12-02 14:57:01 -0600

    Part 2?

--- Day 1/solution.R
+++ Day 1/solution.R
@@ -2,6 +2,7 @@ library(data.table)
 
 input <- data.table::fread("Day 1/input.csv"
                            , header = FALSE)
+# Part 1
 
 param <- input[
   
@@ -13,3 +14,21 @@ param <- input[
 
 param[, check := ifelse(remain %in% value, TRUE, FALSE)]
 solution <- param[check == TRUE][1, solution := value * remain]
+
+# Part 2
+values <- as.vector(input$V1)
+
+differences1 <- as.matrix(outer(values, values, `+`))
+differences2 <- 2020 - differences1
+
+matches <- which(matrix(differences2 %in% values, dim(differences2)), arr.ind = TRUE)
+
+pot_match1 <- as.numeric(matches[1, 1])
+pot_match2 <- as.numeric(matches[2, 1])
+pot_match3 <- as.numeric(matches[3, 1])
+
+match_value1 <- values[pot_match1]
+match_value2 <- values[pot_match2]
+match_value3 <- values[pot_match3]
+
+solution <- match_value1*match_value2*match_value3
