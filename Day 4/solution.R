006441b1dd9330e8387b32eb68a9255eba0e09cc
Tomas Okal <tomas.okal@gmail.com>
2020-12-04 08:38:11 -0600

    all my homies hate strings

--- Day 4/solution.R
+++ Day 4/solution.R
@@ -40,7 +40,8 @@ input_2[, hgt_cm_val := ifelse(substr(hgt_val, 4, 5) == "cm", substr(hgt_val, 1,
 input_2[, hgt_in_val := ifelse(substr(hgt_val, 3, 4) == "in", substr(hgt_val, 1, 2), 0)]
 input_2[, hcl_val := substr(sub(".*hcl:", "", string), 1, 7)]
 input_2[, ecl_val := substr(sub(".*ecl:", "", string), 1, 3)]
-input_2[, pid_val := substr(sub(".*pid:", "", string), 1, 9)]
+input_2[, pid_val1 := sub(".*pid:", "", string)]
+input_2[, pid_val2 := gsub(" .*$", "", pid_val1)]
 
 input_2[, byr_check := ifelse(as.numeric(byr_val) > 1919 & as.numeric(byr_val) < 2003, 1, 0)]
 input_2[, iyr_check := ifelse(as.numeric(iyr_val) > 2009 & as.numeric(iyr_val) < 2021, 1, 0)]
@@ -48,10 +49,9 @@ input_2[, eyr_check := ifelse(as.numeric(eyr_val) > 2019 & as.numeric(eyr_val) <
 input_2[, hgt_cm_check := ifelse(as.numeric(hgt_cm_val) > 149 & as.numeric(hgt_cm_val) < 194, 1, 0)]
 input_2[, hgt_in_check := ifelse(as.numeric(hgt_in_val) > 58 & as.numeric(hgt_in_val) < 77, 1, 0)]
 input_2[, hgt_check := ifelse(hgt_cm_check == 1 | hgt_in_check == 1, 1, 0)]
-input_2[, hcl_check := ifelse(grepl('^#[0-9a-f]{6}$', hcl_val), 1, 0)]
+input_2[, hcl_check := ifelse(grepl('#[0-9a-f]{6}$', hcl_val), 1, 0)]
 input_2[, ecl_check := ifelse(ecl_val %chin% c("amb", "blu", "brn", "gry", "grn", "hzl", "oth"), 1, 0)]
-input_2[, pid_check := ifelse(grepl('^[0-9]{9}$', pid_val), 1, 0)]
+input_2[, pid_check := ifelse(grepl('^[0-9]{9}$', pid_val2), 1, 0)]
 
 input_2[, check := byr_check + iyr_check + eyr_check + hgt_check + hcl_check + ecl_check + pid_check]
-
 solution <- nrow(input_2[check == 7, ])
